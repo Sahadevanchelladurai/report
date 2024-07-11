7ff069c6f992d950e75eb9bf1723d1d6b6157ed8
Sahadevan <sahadevan@gmail.com>
2024-07-11 13:38:54 +0530

    sahadevan

--- src/main/java/com/SRG/report/controller/ReportController.java
+++ src/main/java/com/SRG/report/controller/ReportController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class ReportController {
 
-	@GetMapping("/user123")
+	@GetMapping("/user")
 	public String welcome() {
 		return "Welcome User";
 	}
