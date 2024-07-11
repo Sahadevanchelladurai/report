5c9c9962c0ebcb70bef35cd265b3d98e6563b146
Your Name <you@example.com>
2024-07-11 12:11:41 +0530

    first commit

---
+++ src/main/java/com/SRG/report/controller/ReportController.java
@@ -0,0 +1,19 @@
+package com.SRG.report.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ReportController {
+
+	@GetMapping("/user")
+	public String welcome() {
+		return "Welcome User";
+	}
+	@GetMapping("/admin")
+	public String welcomeAdmin() {
+		return "Welcome Admin ";
+	}
+}
